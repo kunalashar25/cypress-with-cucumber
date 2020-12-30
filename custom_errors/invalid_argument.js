9b825eba5f0ab0e7f7159ab5e40d01339bb414f9
Kunal Ashar <kunalashar@Kunals-MacBook-Pro.local>
2020-12-30 13:46:21 +0800

    description added

--- custom_errors/invalid_argument.js
+++ custom_errors/invalid_argument.js
@@ -1,5 +1,6 @@
 class InvalidArgumentError extends Error {
     /**
+     * @description To throw error when unsupported or invalid data is sent in an argument
      * @param {string} message
      */
     constructor(message) {
